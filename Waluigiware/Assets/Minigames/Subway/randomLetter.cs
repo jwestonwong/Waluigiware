6d2ae1d3ca5fa2e1f655fed6ea2c61a4b7ce9c90
Jason <jwestonwong@gmail.com>
2018-02-14 11:28:56 -0500

    Subway game completed
    Completed the subway game with other fixes - next on the to do list are visual indicators of scoring and point totals.

--- Waluigiware/Assets/Minigames/Subway/randomLetter.cs
+++ Waluigiware/Assets/Minigames/Subway/randomLetter.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class randomLetter : MonoBehaviour {
 
+	private GameManager gM;
+	private bool won = false;
+	private float winTime;
+	private float endWin;
 
 	int buttonCounts = 0;
 	int buttonTarget;
 
-	char letter;
+	public Text text;
+
+	public char [] letters;
+	public char letterCurrent;
 	int letterTotal;
 
 	// Use this for initialization
@@ -19,18 +27,84 @@ public class randomLetter : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		endWin = Time.time - winTime;
+
+		if (letterCurrent == letters [0]) {
+			if (Input.GetKeyDown (KeyCode.Q)) {
+				buttonCounts++;
+				Debug.Log ("Q Pressed");
+			}
+		} else if (letterCurrent == letters [1]) {
+			if (Input.GetKeyDown (KeyCode.W)) {
+				buttonCounts++;
+				Debug.Log ("W Pressed");
+			}
+		} else if (letterCurrent == letters [2]) {
+			if (Input.GetKeyDown (KeyCode.E)) {
+				buttonCounts++;
+				Debug.Log ("E Pressed");
+			}
+		} else if (letterCurrent == letters [3]) {
+			if (Input.GetKeyDown (KeyCode.R)) {
+				buttonCounts++;
+				Debug.Log ("R Pressed");
+			}
+		} else if (letterCurrent == letters [4]) {
+			if (Input.GetKeyDown (KeyCode.A)) {
+				buttonCounts++;
+				Debug.Log ("A Pressed");
+			}
+		} else if (letterCurrent == letters [5]) {
+			if (Input.GetKeyDown (KeyCode.S)) {
+				buttonCounts++;
+				Debug.Log ("S Pressed");
+			}
+		} else if (letterCurrent == letters [6]) {
+			if (Input.GetKeyDown (KeyCode.D)) {
+				buttonCounts++;
+				Debug.Log ("D Pressed");
+			}
+		} else {
+			if (Input.GetKeyDown (KeyCode.F)) {
+				buttonCounts++;
+				Debug.Log ("F Pressed");
+			}
+		}
 
 		if (buttonCounts >= buttonTarget) {
-			
+			newLetter ();
+			buttonCounts = 0;
+			buttonTarget = Random.Range (10, 20);
+			letterTotal++;
+			Debug.Log ("Next Letter");
+		}
+
+		if (letterTotal >= 8) {
+			won = true;
+			winTime = Time.time;
+		}
+
+		if (won) {
+			if (endWin >= 2f) {
+				LevelChange ();
+			}
 		}
 
 	}
 
 	void newLetter (){
-
+		letterCurrent = letters[Random.Range(0,letters.Length)];
+		if (letterTotal < 8) {
+			text.text = (letterCurrent.ToString ());
+		} else {
+			text.text = ("Mouse Won!");
+		}
 	}
 
-	void nextLetter (){
-		
+	void LevelChange (){
+		gM = FindObjectOfType<GameManager> ();
+		gM.keyboardScore++;
+		gM.LevelChange ();
+		Debug.Log ("LevelChange");
 	}
 }
